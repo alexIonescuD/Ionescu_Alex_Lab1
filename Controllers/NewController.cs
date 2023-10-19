fc06bae3c8266b98669ca04b794bdc92867eb7bd
alexIonescuD <dionescu90@yahoo.com>
2023-10-19 20:17:11 +0300

    Ex28

---
+++ Controllers/NewController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ionescu_Alex_Lab1.Controllers
+{
+    public class NewController : Microsoft.AspNetCore.Mvc.Controller
+    {
+
+        public string Index()
+        {
+            return "Wellcome";
+        }
+
+        public string Greeting()
+        {
+            return "Hello!";
+        }
+
+        public string ParameterFunc(string name, int age)
+        {
+            return "Hello, " + name + ", " + age.ToString();
+        }
+    }
+}
